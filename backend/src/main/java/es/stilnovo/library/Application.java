4af77abff6cce1afb0d4fb7fa8ca1c8b12172598
raultejada24 <raulintm@gmail.com>
2026-02-13 21:32:03 +0100

    Comment

--- backend/src/main/java/es/stilnovo/library/Application.java
+++ backend/src/main/java/es/stilnovo/library/Application.java
@@ -1,5 +1,5 @@
 package es.stilnovo.library;
-
+// Es el main de la aplicación, el punto de entrada. Es el encargado de arrancar la aplicación y de cargar el contexto de Spring. Es el encargado de cargar los beans y de configurar la aplicación. Es el encargado de arrancar el servidor web y de escuchar las peticiones. Es el encargado de gestionar el ciclo de vida de la aplicación. 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
